9377e29c0de05d252401f3bb4fc103e47c777e44
王佳 <wangjia@cvte.cn>
2015-02-28 09:17:51 +0800

    添加了屏幕截图

--- app/src/main/java/com/jacob/arcmenu/MainActivity.java
+++ app/src/main/java/com/jacob/arcmenu/MainActivity.java
@@ -1,15 +1,40 @@
 package com.jacob.arcmenu;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+import static android.view.View.OnClickListener;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_arc_menu).setOnClickListener(this);
+        findViewById(R.id.button_custom_menu).setOnClickListener(this);
+        findViewById(R.id.button_le_menu).setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.button_arc_menu:
+                intent = new Intent(MainActivity.this, ArcMenuActivity.class);
+                break;
+            case R.id.button_custom_menu:
+                intent = new Intent(MainActivity.this, SimpleArcMenuActivity.class);
+                break;
+            case R.id.button_le_menu:
+                intent = new Intent(MainActivity.this, LedongliMenuActivity.class);
+                break;
+        }
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/jacob/arcmenu/SimpleArcMenuActivity.java
@@ -0,0 +1,15 @@
+package com.jacob.arcmenu;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class SimpleArcMenuActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_menu);
+    }
+
+}
